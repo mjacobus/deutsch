b780ec0974697dcba5b18eb261e9df3485ee6698
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-06-15 07:31:25 +0200

    Add bundle js back to the asset pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   entry: './frontend/src/app.js',
   output: {
-    path: __dirname + '/public/js/',
+    path: __dirname + '/apps/web/assets/javascripts/',
     library: 'App',
     filename: 'app.bundle.js',
   },
